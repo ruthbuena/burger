2ba261a6a90d3d07e18aa529ff2a2d499e08e991
ruthbuena <ruthbpeace@gmail.com>
2017-08-10 08:35:22 -0400

    Initial File creation/folder set up

---
+++ server.js
@@ -0,0 +1,7 @@
+Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require ("method-override");
+
+var app = express();
+var port = 3000;
