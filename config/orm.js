f6971179c4b1a2670e9362e0cafa4057e1834f1b
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-08-31 09:45:59 -0400

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,81 +1,77 @@
 // Import(require) connection file into this file
 var connection = require ("../config/connection.js");
 
-// Function used for SQL syntax
-function printQuestionMarks(num){
-  var arr = [];
+// Connect to MySQL database
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  };
+  console.log('connected as id ' + connection.threadId);
+});
 
-  for (var i=0; i<num; i++){
-    arr.push("?");
-  }
-  return arr.toString();
-}
-
-// Function used for SQL syntax
-function objToString(ob){
-  var arr =[];
-
-  for (var key in ob) {
-    if(Object.hasOwnProperty.call(ob,key)){
-      arr.push(key + "=" + ob[key]);
-    }
-  }
-  return arr.toString();
-}
-
-// Object used for SQL method
+//ORM
 var orm = {
 
-// Select all method
-  all: function(tableInput, cb){
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err,result){
-      if(err) {
-        throw err;
+  showAll: function(callback) {
+    connection.query('SELECT * FROM burgers', function (err, result) {
+      if (err) throw err;
+      callback(result);
+    });
+
+  },
+
+  // insert burger
+  insertBurger: function(burger_name, callback){
+
+    var d = new Date();
+    var timestamp = ''+ d.getFullYear() + '-';
+    var month = '' + (d.getMonth() + 1);
+      if(month.length == 1){
+        month = '0' + month;
       }
-      cb(result);
+    timestamp += month + '-';
+    var day = '' + d.getDate();
+      if(day.length == 1){
+        day = '0' + day;
+      }
+    timestamp += day + ' ';
+    var hour = '' + d.getHours();
+      if(hour.length == 1){
+        hour = '0' + hour;
+      }
+    timestamp += hour + ':';
+    var minute = '' + d.getMinutes();
+      if(minute.length == 1){
+        minute = '0' + minute;
+      }
+    timestamp += minute + ':';
+    var second = '' + d.getSeconds();
+      if(second.length == 1){
+        second = '0' + second;
+      }
+    timestamp += second;
+
+    // Insert info into DB
+    connection.query('INSERT INTO burgers SET ?', {
+      burger_name: burger_name,
+      devoured: false,
+      date: timestamp
+    }, function (err, result) {
+      if (err) throw err;
+      callback(result);
     });
   },
 
-// Create Method
-  create: function(table, cols, vals, cb){
-    var queryString = "INSERT INTO " + table;
-
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
-
-    console.log(queryString);
-
-    connection.query(queryString, vals, function(err,result){
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-
-// Update Method
-  update: function(table, objColVals, condition, cb){
-    var queryString = "UPDATE " + table;
-
-    queryString += " SET ";
-    queryString += objToString(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-
-    console.log(queryString);
-    connection.query(queryString, function(err,result){
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
+  // update function
+  updateBurger: function(burgerID, callback){
+    connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}], function (err, result) {
+        if (err) throw err;
+        callback(result);
+      });
   }
 };
 
-// Export ORM
+// Export the ORM
 module.exports = orm;
+
